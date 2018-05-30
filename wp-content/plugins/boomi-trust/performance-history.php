e8b49cdb491f139e6c876a975dc5fc17b5cbc541
Erik <erikdmitchell@gmail.com>
2018-05-30 17:03:38 -0400

    tweaked our layout for performance history

--- wp-content/plugins/boomi-trust/performance-history.php
+++ wp-content/plugins/boomi-trust/performance-history.php
@@ -14,9 +14,7 @@ class Boomi_Trust_Performance {
 	public function shortcode($atts) {
 		$html='';
 		$statuses=$this->get_statuses();
-echo '<pre>';
-print_r($statuses);
-echo '</pre>';
+
 		$html.='<div class="container performance">';
 
 			$html.='<div class="row header">';
@@ -34,10 +32,10 @@ echo '</pre>';
 			if (!empty($statuses)) :				
 				foreach ($statuses as $status) :
 					$html.='<div class="row">';
-						$html.='<div class="col-sm-2 date">'.date('M. d, Y', strtotime(get_post_meta($status->ID, '_timestamp', true))).'</div>';
-						$html.='<div class="col-sm-3 circle"><span class="status-circle '.$this->get_status_circle_class($service['atomsphere-platform']).'"></span></div>';
-						$html.='<div class="col-sm-2 circle"><span class="status-circle '.$this->get_status_circle_class($service['atom-cloud']).'"></span></div>';
-						$html.='<div class="col-sm-2 cirlce"><span class="status-circle '.$this->get_status_circle_class($service['mdm-cloud']).'"></span></div>';
+						$html.='<div class="col-sm-2 date"><a href="#">'.date('M. d, Y', strtotime(get_post_meta($status->ID, '_status_timestamp', true))).'</a></div>';
+						$html.='<div class="col-sm-3 circle"><span class="status-circle '.$this->get_status_circle_class($status->statuses['atomsphere-platform']).'"></span></div>';
+						$html.='<div class="col-sm-2 circle"><span class="status-circle '.$this->get_status_circle_class($status->statuses['atom-cloud']).'"></span></div>';
+						$html.='<div class="col-sm-2 cirlce"><span class="status-circle '.$this->get_status_circle_class($status->statuses['mdm-cloud']).'"></span></div>';
 						$html.='<div class="col-sm-3 daily-metrics dm-number"></div>';
 					$html.='</div>';
 				endforeach;
@@ -49,13 +47,40 @@ echo '</pre>';
 	}
 	
     protected function get_statuses() {
+        global $wpdb;
+        
         $statuses = get_posts(array(
            'posts_per_page' => 15,
            'post_type' => 'cloudstatuses',
-           'meta_key' => '_timestamp',
+           'meta_key' => '_status_timestamp',
            'order_by' => 'meta_value',
            'order' => 'DESC', 
         ));
+        
+        if (empty($statuses))
+            return $statuses;
+        
+        // append platforms and statuses.
+        foreach ($statuses as $status) :       
+            $status_arr = array();
+            $final_status = 'operating-normally';
+            $status_statuses = array_keys( get_post_meta($status->ID, '_timestamp_slugs', true) ); 
+            
+            // our keys above give us the plat form, we must find if there's a non operating normally status and set it as final for display.
+            foreach ($status_statuses as $ss) :
+                $status_slug = $wpdb->get_var("SELECT meta_value FROM $wpdb->postmeta WHERE post_id = $status->ID AND meta_key LIKE '_{$ss}%-status'");
+             
+                if ($status_slug != 'operating-normally') :
+                    $final_status = $status_slug; 
+                else :
+                    $final_status = 'operating-normally';
+                endif;
+                   
+                $status_arr[$ss] = $final_status;
+            endforeach;
+
+            $status->statuses = $status_arr; 
+        endforeach;
 
         return $statuses;
 	}
