f23e6d6bc2395997080f25a1d7056245dea9692c
Erik <erikdmitchell@gmail.com>
2018-05-31 10:32:11 -0400

    updated our gulp and composer files

--- wp-content/themes/boomi-trust/gulpfile.js
+++ wp-content/themes/boomi-trust/gulpfile.js
@@ -1,89 +1,229 @@
-(function() {
-	'use strict';
+// Project configuration
+var buildInclude = [
+        // include common file types
+        '**/*.php',
+        '**/*.html',
+        '**/*.css',
+        '**/*.js',
+        '**/*.svg',
+        '**/*.ttf',
+        '**/*.otf',
+        '**/*.eot',
+        '**/*.woff',
+        '**/*.woff2',
 
-	var gulp = require('gulp');
-	var sass = require('gulp-sass');
-	var plumber = require('gulp-plumber');
-	var rename = require('gulp-rename');
-	var gutil = require('gulp-util');
-	var sourcemaps = require('gulp-sourcemaps');
-	var postcss = require('gulp-postcss');
-	var autoprefixer = require('autoprefixer');
-	var cssnano = require('cssnano');
-	var livereload = require('gulp-livereload');
-	var uglify = require('gulp-uglify');
-	//var pump = require('pump');
-	var cssdeclsort = require('css-declaration-sorter');
-	//var browserify = require('browserify');
-	//var tap = require('gulp-tap');
-	//var buffer = require('gulp-buffer');
+        // include specific files and folders
+        'screenshot.png',
 
-	var onError = function(err) {
-		// eslint-disable-next-line no-console
-		console.log('An error ocurred: ', gutil.colors.magenta(err.message));
-		gutil.beep();
-		this.emit('end');
-	}
+        // exclude files and folders
+        '!node_modules/**/*',
+        '!style.css.map',
+        '!assets/js/custom/*',
+        '!assets/css/patrials/*'
 
-	function notifyLiveReload(event) {
-		var fileName = require('path').relative(__dirname, event.path);
-		livereload.changed(fileName);
-	}
+    ];
+    
+var phpSrc = [
+        '**/*.php', // Include all files    
+        '!node_modules/**/*', // Exclude node_modules/
+        '!vendor/**' // Exclude vendor/    
+    ];
 
-	gulp.task('sass-site', function() {
-		var processors = [
-			autoprefixer({browsers: ['last 2 versions']}),
-			cssdeclsort({order: 'alphabetically'}),
-		];
-		return gulp.src('./sass/style.scss')
-			.pipe(plumber({errorHandler: onError}))
-			.pipe(sourcemaps.init())
-			.pipe(sass({ outputStyle: 'nested' }))
-			.pipe(postcss(processors))
-			.pipe(sourcemaps.write())
-			.pipe(rename("style.css"))
-			.pipe(gulp.dest('./'))
-			.pipe(livereload())
-	});
-	
+var cssInclude = [
+        // include css
+        '**/*.css',
+
+        // exclude files and folders
+        '!**/*.min.css',
+        '!node_modules/**/*',
+        '!style.css.map',
+        '!vendor/**'
+    ];
+    
+var jsInclude = [
+        // include js
+        '**/*.js',
+
+        // exclude files and folders
+        '!**/*.min.js',
+        '!node_modules/**/*',
+        '!vendor/**',
+        '!**/gulpfile.js'       
+    ];    
+
+// Load plugins
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'), // Autoprefixing magic
+    minifycss = require('gulp-uglifycss'),
+    filter = require('gulp-filter'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    newer = require('gulp-newer'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    runSequence = require('gulp-run-sequence'),
+    sass = require('gulp-sass'),
+    plugins = require('gulp-load-plugins')({
+        camelize: true
+    }),
+    ignore = require('gulp-ignore'), // Helps with ignoring files and directories in our run tasks
+    plumber = require('gulp-plumber'), // Helps prevent stream crashing on errors
+    cache = require('gulp-cache'),
+    sourcemaps = require('gulp-sourcemaps'),
+    jshint = require('gulp-jshint'), // JSHint plugin
+    stylish = require('jshint-stylish'), // JSHint Stylish plugin
+    stylelint = require('gulp-stylelint'), // stylelint plugin
+    phpcs = require('gulp-phpcs'); // Gulp plugin for running PHP Code Sniffer.
+    phpcbf = require('gulp-phpcbf'); // PHP Code Beautifier
+    gutil = require('gulp-util'); // gulp util
+
+/**
+ * Styles
+ */
+ 
+// compile sass
+gulp.task('sass', function () {
+    gulp.src('**/sass/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'nested',
+            precision: 10
+        }))
+        .pipe(sourcemaps.write({
+            includeContent: false
+        }))
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('./'))
+});
+
+// minify all css
+gulp.task('mincss', function () {
+    gulp.src(cssInclude)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write({
+            includeContent: false
+        }))
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(plumber.stop())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss({
+            maxLineLen: 80
+        }))
+        .pipe(gulp.dest('./'))
+});
+
+// css linting with Stylelint.
+gulp.task('lintcss', function lintCssTask() {
+  return gulp.src(cssInclude)
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});	
+
+/**
+ * Scripts
+ */
+
+// min all js files
+gulp.task('scripts', function () {
+    return gulp.src(jsInclude)
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./'))
+});
+
+// js linting with JSHint.
+gulp.task('lintjs', function() {
+  return gulp.src(jsInclude)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
 /*
-	gulp.task('sass-site', function() {
-		var processors = [
-			autoprefixer({stats: ['> 1%']}),
-			cssdeclsort({order: 'alphabetically'}),
-			cssnano(),
-		];
-		return gulp.src('./sass/style.scss')
-			.pipe(plumber({errorHandler: onError}))
-			.pipe(sourcemaps.init())
-			.pipe(sass({ outputStyle: 'nested' }))
-			.pipe(postcss(processors))
-			.pipe(rename("style.css"))
-			.pipe(gulp.dest('./'))
-	});
-*/
-/*
-	gulp.task('sass-admin', function() {
-		var processors = [
-			autoprefixer({browsers: ['last 2 versions']}),
-		];
-		return gulp.src('./sass/admin.sass')
-			.pipe(plumber({errorHandler: onError}))
-			.pipe(sass({ outputStyle: 'nested' }))
-			.pipe(postcss(processors))
-			.pipe(gulp.dest('./css/'))
-	});
+gulp.task('scripts', function () {
+    return gulp.src('./js/*.js')
+        .pipe(concat('custom.js'))
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(rename({
+            basename: "custom",
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/js/'))
+        .pipe(notify({
+            message: 'Custom scripts task complete',
+            onLast: true
+        }));
+});
 */
 
-	gulp.task('watch', ['sass'], function() {		
-		livereload.listen();
-		gulp.watch('./sass/**/*.scss', ['sass']);
-		gulp.watch('./sass/**/*.sass', ['sass']);
-		gulp.watch('./**/*.php', notifyLiveReload);
-		gulp.watch('./js/src/*.js', ['js']);
-	});
+/**
+ * PHP
+ */
 
-	gulp.task('sass', ['sass-site']);
-	gulp.task('default', ['sass']);
-}());
+// PHP Code Sniffer.
+gulp.task('phpcs', function () {
+    return gulp.src(phpSrc)
+        // Validate files using PHP Code Sniffer
+        .pipe(phpcs({
+            bin: 'vendor/bin/phpcs',
+            standard: './phpcs.ruleset.xml',
+            warningSeverity: 0
+        }))
+        .pipe(phpcs.reporter('log')); // Log all problems that was found
+});
 
+// PHP Code Beautifier.
+gulp.task('phpcbf', function () {
+    return gulp.src(phpSrc)
+        .pipe(phpcbf({
+            bin: 'vendor/bin/phpcbf',
+            standard: './phpcs.ruleset.xml',
+            warningSeverity: 0
+        }))       
+        .on('error', gutil.log)
+        .pipe(gulp.dest('./'));
+});
+
+// ==== TASKS ==== //
+/**
+ * Gulp Default Task
+ *
+ * Compiles styles, watches js and php files.
+ *
+ */
+
+// Package Distributable - sort of
+gulp.task('build', function (cb) {
+    runSequence('styles', 'scripts', cb);
+});
+
+// Styles task
+gulp.task('styles', function (cb) {
+    runSequence('sass', 'mincss', cb);
+});
+
+
+// Watch Task
+gulp.task('default', ['styles', 'scripts'], function () {
+    gulp.watch('./sass/**/*', ['sass']);
+    gulp.watch('./js/**/*.js', ['scripts']);
+});
