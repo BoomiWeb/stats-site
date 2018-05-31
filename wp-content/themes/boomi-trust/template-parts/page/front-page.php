d79e57846c2380579d671f065244635b287e5c9d
Erik <erikdmitchell@gmail.com>
2018-05-31 13:38:48 -0400

    removed clous statuses from home page

--- wp-content/themes/boomi-trust/template-parts/page/front-page.php
+++ wp-content/themes/boomi-trust/template-parts/page/front-page.php
@@ -1,19 +1,6 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-7">
-			
-			<div class="row cloud-statuses">
-			
-				<?php foreach (boomi_trust_cloud_statuses() as $status) : ?>
-					<div class="col-md-4">
-						<div class="cloud-status <?php boomi_trust_cloud_status_class($status->ID); ?>">
-							<div class="service"><?php boomi_trust_cloud_service($status->ID); ?></div>
-							<div class="status"><?php boomi_trust_cloud_status($status->ID); ?></div>
-						</div>
-					</div>
-				<?php endforeach; ?>			
-			</div>
-	
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
