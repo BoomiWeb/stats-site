29e78f6da85befaf6b1100bb9aada9b375e0efce
Erik <erikdmitchell@gmail.com>
2018-05-31 16:50:01 -0400

    changed logo

--- wp-content/themes/boomi-trust/template-parts/navigation/navbar-brand.php
+++ wp-content/themes/boomi-trust/template-parts/navigation/navbar-brand.php
@@ -1,7 +1,7 @@
 <div class="boomi-trust-header-wrap">
 
 	<div class="boomi-trust-header-image">
-		<a href="<?php echo site_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/images/boomi-logo.svg" height="" width="" alt="boomi-logo" /></a>
+		<a href="<?php echo site_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/images/boomi-logo-white.svg" height="" width="" alt="boomi-logo" /></a>
 	</div>
 		
 </div>
