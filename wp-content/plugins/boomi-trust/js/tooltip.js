cd69b56dbe6592921e1c99ec02acae4ef8d818aa
Erik <erikdmitchell@gmail.com>
2018-05-31 15:21:37 -0400

    removed performance history files

--- wp-content/plugins/boomi-trust/js/tooltip.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    $('.simcoe-calendar .trust-cal-icon').tooltip();
-});
-
-jQuery(document).bind('simcoe_calendar_ajax_load', function(event, response) {
-	jQuery('.simcoe-calendar .trust-cal-icon').tooltip();
-});
