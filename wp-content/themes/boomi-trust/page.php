56afc3b6728a14d5061b708eaebf94dc27b22320
Erik <erikdmitchell@gmail.com>
2018-05-31 16:49:43 -0400

    added title to page

--- wp-content/themes/boomi-trust/page.php
+++ wp-content/themes/boomi-trust/page.php
@@ -10,7 +10,13 @@
 	
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-xs-12 heading-title">
+    			<?php the_title('<h1>', '</h1>'); ?>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
 	
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part('template-parts/page/content', boomi_trust_page_slug(get_the_ID())); ?>
