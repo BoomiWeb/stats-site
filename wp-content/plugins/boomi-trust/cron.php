da4585fda9494d937bfcad94b4ac3913d3a54390
Erik <erikdmitchell@gmail.com>
2018-06-06 14:54:22 -0400

    tweaked cron job

--- wp-content/plugins/boomi-trust/cron.php
+++ wp-content/plugins/boomi-trust/cron.php
@@ -7,7 +7,7 @@
  * @return void
  */
 function boomi_trust_cron_jobs() {
-	$update_statistics=new Boomi_Trust_Update_Statistics();
-	$update_daily_integrations=new Boomi_Trust_Update_Daily_Integrations();
+	Boomi_Trust_Update_Statistics();
+	Boomi_Trust_Update_Daily_Integrations();
 }
 add_action('boomi_trust_statistics_cron_run', 'boomi_trust_cron_jobs');	
