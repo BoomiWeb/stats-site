c6b9b89953778cda8c9f36c2fc84824e64cb722c
Erik <erikdmitchell@gmail.com>
2018-06-13 10:28:13 -0400

    debugging cron

--- wp-content/plugins/boomi-trust/cron.php
+++ wp-content/plugins/boomi-trust/cron.php
@@ -6,10 +6,12 @@
  * @access public
  * @return void
  */
-function boomi_trust_cron_jobs() {        
+function boomi_trust_cron_jobs() {
+    php_custom_logger()->log('begin cron jobs');
+            
 	$update_statistics=new Boomi_Trust_Update_Statistics();
 	$update_daily_integrations=new Boomi_Trust_Update_Daily_Integrations();
 	
-    wp_mail('emitchell@boomi.com', 'cron report', 'crons run');
+    php_custom_logger()->log('end cron jobs');
 }
 add_action('boomi_trust_statistics_cron_run', 'boomi_trust_cron_jobs');	
