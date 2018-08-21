9c9914074c7da18ef82efd216a50c89607ebf164
Erik <erikdmitchell@gmail.com>
2018-08-21 17:35:02 -0400

    edit release date func

--- wp-content/plugins/boomi-trust/functions.php
+++ wp-content/plugins/boomi-trust/functions.php
@@ -31,26 +31,10 @@ add_filter( 'upload_mimes', 'boomi_mime_types' );
 
 function boomi_pc_get_rcd_dates() {
     global $wpdb;
+    
+    $post = get_page_by_title('Release Control Date', OBJECT, 'release');
 
-    $today = date( 'Y-m-d' );
-    $date = date( 'Y-m-d', strtotime( "$today -1 year" ) );
-    $dates = $wpdb->get_col(
-        "
-        SELECT $wpdb->postmeta.meta_value
-        FROM $wpdb->posts
-        INNER JOIN $wpdb->postmeta ON ( $wpdb->posts.ID = $wpdb->postmeta.post_id )
-        INNER JOIN $wpdb->term_relationships ON ($wpdb->posts.ID = $wpdb->term_relationships.object_id)
-        INNER JOIN $wpdb->term_taxonomy ON ($wpdb->term_relationships.term_taxonomy_id = $wpdb->term_taxonomy.term_taxonomy_id)
-        INNER JOIN $wpdb->terms ON ($wpdb->terms.term_id = $wpdb->term_taxonomy.term_id)        
-        WHERE $wpdb->posts.post_type = 'pcevent'
-            AND $wpdb->posts.post_status = 'publish'
-            AND ( $wpdb->postmeta.meta_key LIKE '_start_date_%' AND CAST($wpdb->postmeta.meta_value AS DATE) >= '{$date}')
-            AND $wpdb->terms.slug = 'release-control-date'
-        ORDER BY $wpdb->postmeta.meta_value ASC
-    "
-    );
-
-    return $dates;
+    print_r($post);
 }
 
 function boomi_pc_get_rd() {
