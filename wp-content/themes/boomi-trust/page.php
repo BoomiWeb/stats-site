fa3a7ad6a02ce8dccc7616e30139de1ee351f517
Erik <erikdmitchell@gmail.com>
2018-11-27 10:17:26 -0500

    bug fix

--- wp-content/themes/boomi-trust/page.php
+++ wp-content/themes/boomi-trust/page.php
@@ -19,7 +19,7 @@
 			<div class="col-xs-12">
 	
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part('template-parts/page/', 'content'; ?>
+					<?php get_template_part('template-parts/page/', 'content'); ?>
 				<?php endwhile; else: ?>
 					<p><?php _e('Sorry, this page does not exist.', 'boomi-trust'); ?></p>
 				<?php endif; ?>
