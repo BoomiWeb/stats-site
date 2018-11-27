9bc779a883e7f0ed8ceb321982ce58dfe0689a7b
Erik <erikdmitchell@gmail.com>
2018-11-27 10:53:39 -0500

    removed unused code

--- wp-content/themes/boomi-trust/template-parts/page/content.php
+++ wp-content/themes/boomi-trust/template-parts/page/content.php
@@ -7,12 +7,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<!--
-	<header class="entry-header">
-		<?php //the_title( '<h1 class="entry-title">', '</h1>'); ?>
-	</header>
-	-->
-
 	<div class="entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'boomi-trust' ) ); ?>
 	</div><!-- .entry-content -->
