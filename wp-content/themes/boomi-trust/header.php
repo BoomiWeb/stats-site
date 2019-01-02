6b6921271685d4944230d409970c5752d485b84e
Erik <erikdmitchell@gmail.com>
2019-01-02 14:07:11 -0500

    removed hamburger menu

--- wp-content/themes/boomi-trust/header.php
+++ wp-content/themes/boomi-trust/header.php
@@ -24,12 +24,7 @@
 			<div class="container">
 				<nav class="navbar navbar-default" role="navigation">
 					<div class="navbar-header">
-    					<div class="navbar-logo">
-        					<a href="https://status.boomi.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/images/boomi-logo-white.svg" height="" width="" alt="boomi-logo" /></a>
-    					</div>
-    					<div class="navbar-button">
-    						<?php get_template_part('template-parts/navigation/navbar', 'button'); ?>
-    					</div>
+        				<a href="https://status.boomi.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/images/boomi-logo-white.svg" height="" width="" alt="boomi-logo" /></a>
 					</div>
 					<?php get_template_part('template-parts/navigation/primary', 'menu'); ?>
 				</nav>
