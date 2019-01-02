eb83d4db44e4e72234a24e30ff814fd9879220be
Erik <erikdmitchell@gmail.com>
2019-01-02 11:07:37 -0500

    wrapped button in div

--- wp-content/themes/boomi-trust/template-parts/navigation/navbar-button.php
+++ wp-content/themes/boomi-trust/template-parts/navigation/navbar-button.php
@@ -1,6 +1,8 @@
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".boomi-trust-mobile-menu">
-	<span class="sr-only">Toggle navigation</span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-</button>
+<div class="navbar-button">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".boomi-trust-mobile-menu">
+    	<span class="sr-only">Toggle navigation</span>
+    	<span class="icon-bar"></span>
+    	<span class="icon-bar"></span>
+    	<span class="icon-bar"></span>
+    </button>
+</div>
