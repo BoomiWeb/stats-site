739d4b5cba1bea127681c648ec054399338df616
Erik <erikdmitchell@gmail.com>
2019-01-02 14:06:14 -0500

    disabled collapse of primary nav on tablet

--- wp-content/themes/boomi-trust/template-parts/navigation/primary-menu.php
+++ wp-content/themes/boomi-trust/template-parts/navigation/primary-menu.php
@@ -1,11 +1,9 @@
-<div class="collapse navbar-collapse primary-menu">
+<div class="navbar-collapse primary-menu">
 	<?php
 	wp_nav_menu(array(
 		'theme_location' => 'primary',
 		'container' => false,
 		'menu_class' => 'nav navbar-nav pull-left',
-		//'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
-		//'walker' => new wp_bootstrap_navwalker()
 	));
 	?>
 </div> <!-- .primary-menu -->
