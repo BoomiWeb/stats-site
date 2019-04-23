216316eab15851d1b224fae1eecd2fe0d20bc9e9
Erik <erikdmitchell@gmail.com>
2019-04-23 16:34:00 -0400

    boomi security headers plugin

---
+++ wp-content/plugins/boomi-security-headers/README.txt
@@ -0,0 +1,33 @@
+=== Boomi Security Headers ===
+Contributors: erikdmitchell
+Donate link: https://boomi.com
+Tags: headers, security
+Requires at least: 3.0.1
+Tested up to: 5.1.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Security headers for Boomi WordPress websites.
+
+== Description ==
+
+Security headers for Boomi WordPress websites.
+
+== Installation ==
+
+1. Upload `boomi-security-headers.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial version
+
+== Upgrade Notice ==
---
+++ wp-content/plugins/boomi-security-headers/boomi-security-headers.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Plugin Name:       Boomi Security Headers
+ * Plugin URI:        https://boomi.com
+ * Description:       Security headers for Boomi WordPress websites.
+ * Version:           1.0.0
+ * Author:            Erik Mitchell
+ * Author URI:        https://boomi.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       boomi-security-headers
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ */
+define( 'BOOMI_SECURITY_HEADERS_VERSION', '1.0.0' );
+
+/**
+*
+* Add the HSTS header while rendering a response.
+*
+**/
+function boomi_add_header_hsts() {
+    //Content-Security-Policy
+    //Strict-Transport-Security    
+    header( 'Strict-Transport-Security: max-age=31536000;' );
+    
+    //X-Frame-Options
+    header( 'X-Frame-Options: DENY' );
+    
+    //X-XSS-Protection
+    header( 'X-XSS-Protection: 1; mode=block' );
+    
+    //X-Content-Type-Options
+    header( 'X-Content-Type-Options: nosniff' );
+}
+add_action( 'send_headers', 'boomi_add_header_hsts' );
