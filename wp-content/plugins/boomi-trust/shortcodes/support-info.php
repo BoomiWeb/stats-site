2e6a0df05831cdc3d052c4388b4b5761c5b87f84
Erik <erikdmitchell@gmail.com>
2020-02-29 08:14:12 -0500

    updated support info numbers

--- wp-content/plugins/boomi-trust/shortcodes/support-info.php
+++ wp-content/plugins/boomi-trust/shortcodes/support-info.php
@@ -16,8 +16,8 @@ function boomi_stats_support_info( $atts ) {
         $html .= '<div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-4">';
             $html .= '<div class="support-info">';
                 $html .= '<div class="report-text">To report <span>Severity 1</span> issues, please call:</div>';
-                $html .= '<div class="phone-number"><a href="tel:1-866-407-6599">1-866-407-6599</a> (Toll free in US)</div>';
-                $html .= '<div class="phone-number"><a href="tel:1-503-470-5056">1-503-470-5056</a> (International)</div>';
+                $html .= '<div class="phone-number"><a href="tel:1-888-297-5808">1-888-297-5808</a> (Toll free in US)</div>';
+                $html .= '<div class="phone-number"><a href="tel:1-512-513-7117">1-512-513-7117</a> (International)</div>';
             $html .= '</div>';
         $html .= '</div>';
 
