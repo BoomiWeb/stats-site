48a2f21594ccefca3f4accf34c73b0692570f892
Erik Mitchell <erikdmitchell@gmail.com>
2024-12-11 13:35:21 -0500

    added query monitor plugin

--- wp-content/themes/boomi-trust/page.php
+++ wp-content/themes/boomi-trust/page.php
@@ -7,16 +7,16 @@
 ?>
 
 <?php get_header(); ?>
-	
+
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 heading-title">
+			<div class="col-12 heading-title">
     			<?php the_title('<h1>', '</h1>'); ?>
 			</div>
 		</div>
 
 		<div class="row">
-			<div class="col-xs-12">
+			<div class="col-12 col-md-6">
 	
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part('template-parts/page/content', ''); ?>
